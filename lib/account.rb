8bdd634547eb92115f25b1a8cd36e0019d9258a0
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-11 14:50:52 +0000

    feat: balance can be increased with deposit method

--- lib/account.rb
+++ lib/account.rb
@@ -1,9 +1,17 @@
 class Account
 
-  attr_accessor :balance
+  
 
   def initialize
     @balance = [0]
+  end
+
+  def balance
+    @balance.reduce(:+)
+  end
+
+  def deposit(amount)
+    @balance << amount
   end
 
 end
--- spec/units/model_spec.rb
+++ spec/units/model_spec.rb
@@ -3,11 +3,11 @@ require 'account'
 describe 'Account' do  
   account = Account.new
     it 'has a 0 starting balance' do
-      expect(account.balance).to eq([0])
+      expect(account.balance).to eq(0)
     end
     it 'balance can be increased with #deposit' do
       account.deposit(10)
-      expect(account.balance).to eq([10])
+      expect(account.balance).to eq(10)
     end
 end
 
