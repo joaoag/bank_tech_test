14f2004c1da0289fd84bfb2fbc6df65bc6336cf7
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-11 14:42:17 +0000

    test: failing unit test write for account balance starting at 0

--- spec/units/model_spec.rb
+++ spec/units/model_spec.rb
@@ -1,24 +1,9 @@
-require 'model'
+require 'account'
+
+describe 'Account' do  
+    it 'has a 0 starting balance' do
+      expect(account.balance).to eq(0)
+    end
+end
 
 
-
-
-# pure RSpec
-
-# describe 'Airport' do 
-#   let(:airport) { described_class.new }    
-#   let(:plane) { double(:plane) }
-
-#   scenario 'and plane interactions' do
-
-#     before do
-#       allow(weather).to receive(:stormy?).and_return(false)
-#     end
-
-#     it 'responds to #land' do
-#       expect(airport).to respond_to(:land).with(1).argument
-#     end
-
-#   end
-
-# end
