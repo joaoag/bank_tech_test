799e916b48a0fc4832d0c58eaa76cac3c01b6085
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-11 14:52:02 +0000

    test: write failing test for withdrawal method

--- spec/units/model_spec.rb
+++ spec/units/model_spec.rb
@@ -5,8 +5,13 @@ describe 'Account' do
     it 'has a 0 starting balance' do
       expect(account.balance).to eq(0)
     end
-    it 'balance can be increased with #deposit' do
+    it 'balance can be increased with #deposit()' do
       account.deposit(10)
+      expect(account.balance).to eq(10)
+    end
+    it 'balance can be reduced with #withdrawal()' do
+      account.deposit(10)
+      account.withdrawal(10)
       expect(account.balance).to eq(10)
     end
 end
