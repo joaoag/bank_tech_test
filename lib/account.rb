0d150a50bc452e552dea252db760023eec80085a
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-11 14:47:07 +0000

    feat: account has starting balance 0

---
+++ lib/account.rb
@@ -0,0 +1,9 @@
+class Account
+
+  attr_accessor :balance
+
+  def initialize
+    @balance = [0]
+  end
+
+end
--- spec/units/model_spec.rb
+++ spec/units/model_spec.rb
@@ -1,8 +1,9 @@
 require 'account'
 
 describe 'Account' do  
+  account = Account.new
     it 'has a 0 starting balance' do
-      expect(account.balance).to eq(0)
+      expect(account.balance).to eq([0])
     end
 end
 
