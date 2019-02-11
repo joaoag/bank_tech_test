acb283b740900400910eb659012686dd3edba061
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-11 14:47:58 +0000

    test: failing test for desposit method write

--- spec/units/model_spec.rb
+++ spec/units/model_spec.rb
@@ -5,6 +5,10 @@ describe 'Account' do
     it 'has a 0 starting balance' do
       expect(account.balance).to eq([0])
     end
+    it 'balance can be increased with #deposit' do
+      account.deposit(10)
+      expect(account.balance).to eq([10])
+    end
 end
 
 
