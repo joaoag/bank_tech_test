1a32ad996feb831f295a7dc039e33c1cbef60030
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-11 16:24:04 +0000

    feat: account stores the date of deposits

--- lib/account.rb
+++ lib/account.rb
@@ -1,19 +1,31 @@
 class Account
 
+attr_accessor :balance, :transactions
+
   def initialize
     @balance = [0]
+    @transactions = []
   end
 
-  def balance
-    @balance.reduce(:+)
+  def display_balance
+    calculate_balance
+    p @balance.flatten.inject(:+)
   end
-
-  def deposit(amount)
-    @balance << amount
+  
+  def deposit(amount, date)
+    @transactions << { date => amount }
   end
 
   def withdrawal(amount)
     @balance << -amount
   end
 
-end
+private 
+
+  def calculate_balance
+    @transactions.each do | hash |
+    @balance << hash.values
+    end
+  end
+
+end 
--- spec/units/account_spec.rb
+++ spec/units/account_spec.rb
@@ -5,18 +5,18 @@ describe 'Account balance' do
   account = Account.new
   
   it 'has a 0 starting balance' do
-    expect(account.balance).to eq(0)
+    expect(account.balance).to eq([0])
   end
   
   it 'balance can be increased with #deposit()' do
-    account.deposit(10)
-    expect(account.balance).to eq(10)
+    account.deposit(10, "10/10/2020")
+    expect(account.display_balance).to eq(10)
   end
   
   it 'balance can be reduced with #withdrawal()' do
-    account.deposit(10)
+    account.deposit(10, "10/10/2020")
     account.withdrawal(10)
-    expect(account.balance).to eq(10)
+    expect(account.display_balance).to eq(10)
   end
 
 end
@@ -26,8 +26,8 @@ describe 'Account balance with dates' do
   account = Account.new
   
   it 'records the date of deposits' do
-    account.deposit(10, Date.new)
-    expect(account.balance[0]).to eq(Date.new)
+    account.deposit(10, "10/10/2018")
+    expect(account.transactions).to eq(["10/10/2018"=>10])
   end
 
 end
