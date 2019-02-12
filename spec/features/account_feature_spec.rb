818285e8f55fc5ea7a7d12a69992635a629c03bf
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-12 15:18:01 +0000

    test: failing feature test for transaction class

--- spec/features/account_feature_spec.rb
+++ spec/features/account_feature_spec.rb
@@ -1,18 +1,20 @@
 require 'account'
 require 'statement'
 
-describe 'Feature: Account and Statement' do
+describe 'Feature: Account, Transaction and Statement' do
 
   context 'record and display' do
     
     expected_statement_output = "date || credit || debit || balance\n14/01/2012 || || -500.00 || 2500.00\n13/01/2012 || 2000.00 || 3000.00\n10/01/2012 || 1000.00 || 1000.00\n"
     let(:account) { Account.new }
+    let(:debit) {Transaction.new(1000, "10/01/2012")}
     let(:statement) { Statement.new }
+    let(:credit) {Transaction.new(500, "14/01/2012")}
     
     it 'transactions, their dates and resulting balance' do
-      account.deposit(1000, "10/01/2012")
-      account.deposit(2000, "13/01/2012")
-      account.withdrawal(500, "14/01/2012")
+      account.deposit(debit)
+      account.deposit(debit)
+      account.withdrawal(credit)
       expect { statement.print_transactions(account.statement) }.to output(expected_statement_output).to_stdout
     end
   end
