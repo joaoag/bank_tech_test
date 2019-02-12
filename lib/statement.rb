5005cd5df723767fa0e9b8f76ab9eebd047a7e8c
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-12 12:52:51 +0000

    refactor: #print_transactions functionality partially farmed out to #hash_formatter

--- lib/statement.rb
+++ lib/statement.rb
@@ -7,14 +7,18 @@ class Statement
   def print_transactions(statement)
     headers
     statement.reverse.each do |hash|
-      hash.each do |k, v| 
+    hash_formatter(hash)
+    end
+  end
+
+  def hash_formatter(hash)
+    hash.each do |k, v| 
         if (v[0]).positive?
           puts  "#{k} || #{'%.2f' %v[0]} || #{'%.2f' % v[1]}"
         else
           puts  "#{k} || || #{'%.2f' %v[0]} || #{'%.2f' %v[1]}"
         end
       end
-    end
   end
 
 end
