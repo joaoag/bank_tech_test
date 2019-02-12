db975dfdb38d486a80e413adc0401b677f699ef0
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-12 12:18:05 +0000

    refactor: expanding test descriptions

--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -9,11 +9,11 @@ describe 'Statement' do
     expect { statement.headers }.to output("date || credit || debit || balance\n").to_stdout
   end
 
-  it 'displays single deposit transaction' do
+  it 'displays single deposit transaction with date and balance' do
     expect { statement.print_transactions(single_deposit) }.to output("date || credit || debit || balance\n10/10/2020 || 10.00 || 10.00\n").to_stdout
   end
 
-  it 'displays single withdrawal transaction' do
+  it 'displays single withdrawal transaction with date and balance' do
     expect { statement.print_transactions(single_withdrawal) }.to output("date || credit || debit || balance\n10/10/2020 || || -5.00 || 5.00\n").to_stdout
   end
 
