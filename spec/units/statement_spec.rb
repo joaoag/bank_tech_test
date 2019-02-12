2da9aaff14a9469b56c7ad51dab8a3496b358ae4
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-12 12:01:10 +0000

    refactor: numbers display to two decimal points

--- spec/units/statement_spec.rb
+++ spec/units/statement_spec.rb
@@ -2,19 +2,19 @@ require 'statement'
 
 describe 'Statement' do
   statement = Statement.new
-  single_deposit = [{ "10/10/2020" => [10.00, 10.00] }]
-  single_withdrawal = [{ "10/10/2020" => [-5.00, 5.00] }]
+  single_deposit = [{ "10/10/2020" => [10, 10] }]
+  single_withdrawal = [{ "10/10/2020" => [-5, 5] }]
 
   it 'displays column headers' do
     expect { statement.headers }.to output("date || credit || debit || balance\n").to_stdout
   end
 
   it 'displays single deposit transaction' do
-    expect { statement.print_transactions(single_deposit) }.to output("date || credit || debit || balance\n10/10/2020 || 10.0 || 10.0\n").to_stdout
+    expect { statement.print_transactions(single_deposit) }.to output("date || credit || debit || balance\n10/10/2020 || 10.00 || 10.00\n").to_stdout
   end
 
   it 'displays single withdrawal transaction' do
-    expect { statement.print_transactions(single_withdrawal) }.to output("date || credit || debit || balance\n10/10/2020 || || -5.0 || 5.0\n").to_stdout
+    expect { statement.print_transactions(single_withdrawal) }.to output("date || credit || debit || balance\n10/10/2020 || || -5.00 || 5.00\n").to_stdout
   end
 
 end
