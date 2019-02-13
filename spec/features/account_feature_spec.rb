4662a3f661517da4fdf9cc9a2590ce40f54da3af
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-02-13 09:47:29 +0000

    test: failing feature for integrating new transaction class with account and statement classes

--- spec/features/account_feature_spec.rb
+++ spec/features/account_feature_spec.rb
@@ -8,11 +8,14 @@ describe 'Feature: Account and Statement' do
     expected_statement_output = "date || credit || debit || balance\n14/01/2012 || || -500.00 || 2500.00\n13/01/2012 || 2000.00 || 3000.00\n10/01/2012 || 1000.00 || 1000.00\n"
     let(:account) { Account.new }
     let(:statement) { Statement.new }
-    
+    let (:deposit_1k) { Transaction.new(1000) }
+    let (:deposit_2k) { Transaction.new(2000) }
+    let (:withdrawal_500) { Transaction.new(500) }
+
     it 'transactions, their dates and resulting balance' do
-      account.deposit(1000, "10/01/2012")
-      account.deposit(2000, "13/01/2012")
-      account.withdrawal(500, "14/01/2012")
+      account.deposit(deposit_1k)
+      account.deposit(deposit_2k)
+      account.withdrawal(withdrawal_500)
       expect { statement.print_transactions(account.statement) }.to output(expected_statement_output).to_stdout
     end
   end
