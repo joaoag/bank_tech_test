68127944cf3d78b7be2e1fe557f7be70b5cd7f80
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-04-10 12:37:12 +0100

    refactor: removing direct reference to instance variables in Account class

--- lib/account.rb
+++ lib/account.rb
@@ -10,25 +10,25 @@ class Account
 
   def return_balance
     calculate_balance
-    @balance.flatten.inject(:+)
+    balance.flatten.inject(:+)
   end
     
   def deposit(transaction)    
-    @transactions << { transaction.date => transaction.amount }
-    @statement << { transaction.date => [transaction.amount, return_balance] }
+    transactions << { transaction.date => transaction.amount }
+    statement << { transaction.date => [transaction.amount, return_balance] }
   end
 
   def withdrawal(transaction)
-    @transactions << { transaction.date => - transaction.amount }
-    @statement << { transaction.date => [-transaction.amount, return_balance] }
+    transactions << { transaction.date => - transaction.amount }
+    statement << { transaction.date => [-transaction.amount, return_balance] }
   end
 
   private 
 
   def calculate_balance
     @balance = [0]
-    @transactions.each do |hash|
-      @balance << hash.values
+    transactions.each do |hash|
+      balance << hash.values
     end
   end
 end 
